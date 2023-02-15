902de53a4975be7ac03f981971cd57b9d3f5a702
M4488 <maksims.turcins@gmail.com>
2023-02-15 21:20:54 +0200

    Single responsibility p

---
+++ src/main/java/single_responsibility/Main.java
@@ -0,0 +1,39 @@
+package single_responsibility;
+
+public class Main {
+    public static void main(String[] args) {
+EmployeeFacade employeeFacade = new EmployeeFacade();
+employeeFacade.cleanFloor();
+employeeFacade.cook();
+    }
+}
+interface Employee {
+    int getSalary();
+}
+class Cook implements Employee{
+    @Override
+    public int getSalary() {
+        return 0;
+    }
+
+    void cook() {}
+}
+class Janitor implements Employee {
+    @Override
+   public int getSalary() {
+        return 0;
+    }
+
+    void cleanFloor() {}
+}
+
+class EmployeeFacade {
+    Cook cook = new Cook();
+    Janitor janitor = new Janitor();
+    void cook() {
+    cook.cook();
+}
+        void cleanFloor() {
+    janitor.cleanFloor();
+    }
+}
